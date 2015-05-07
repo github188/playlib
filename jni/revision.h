6e716469f71de5e386a5ddb92fadb5a058b9a035
liukp <liukp@email.com>
2015-05-07 15:22:38 +0800

    更换网络库版本号

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[e83af73]"
-#define RELEASE_DATE  "[2015-05-06]"
+#define REVISION      "[c0e340e]"
+#define RELEASE_DATE  "[2015-05-07]"
