7375d0835e0daf132cd51675a30227e8bb4f4997
liukp <liukp@email.com>
2015-05-18 11:03:41 +0800

    生成版本号

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[6e71646]"
-#define RELEASE_DATE  "[2015-05-07]"
+#define REVISION      "[fac4297]"
+#define RELEASE_DATE  "[2015-05-18]"
