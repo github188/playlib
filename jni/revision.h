2994fe36415ea6f6cd50bc3b546cb54004d4dc5a
miaofaqiang <mfq@jovision.com>
2015-06-04 10:00:22 +0800

    修改版本号：[6d22b24]

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[6fffaef]"
-#define RELEASE_DATE  "[2015-06-03]"
+#define REVISION      "[6d22b24]"
+#define RELEASE_DATE  "[2015-06-04]"
