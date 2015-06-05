03599d4042de2489eb0df48c694bb5a804cfdd9b
miaofaqiang <mfq@jovision.com>
2015-06-05 09:42:57 +0800

    modify broadcast 休息频率

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[6d22b24]"
-#define RELEASE_DATE  "[2015-06-04]"
+#define REVISION      "[2994fe3]"
+#define RELEASE_DATE  "[2015-06-05]"
