318d05a62afd0edced5398b6d4a758afebd7c5f8
liukp <liukp@email.com>
2015-06-09 11:50:51 +0800

    修改TextData回调最后一个case的bug

--- jni/utils/callbacks.cc
+++ jni/utils/callbacks.cc
@@ -1396,15 +1396,18 @@ void TextData(int index, BYTE type, BYTE* buf, int size) {
 
 						delete gbk;
 						values["wifi"] = list;
-						break;
 					}
+					break;
                     case 0x01://发送测试邮件
-                        flag = 0x07;
-						char msg[_extends->nParam1 + 1];
-						sprintf(msg, _extends->acData, _extends->nParam1);
-						msg[_extends->nParam1] = '\0';
-						LOGV("transparent: %s", msg);
-						values["msg"] = msg;
+                        {
+                            flag = 0x07;
+                            char msg[_extends->nParam1 + 1];
+                            sprintf(msg, _extends->acData, _extends->nParam1);
+                            msg[_extends->nParam1] = '\0';
+                            LOGV("transparent: %s", msg);
+                            values["msg"] = msg;
+                        }
+
                         break;
 					default:
 						LOGE(
