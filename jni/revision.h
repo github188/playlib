306cf314d5612fc285bce0b4066ab0ccdda257f2
miaofaqiang <mfq@jovision.com>
2015-06-17 14:46:40 +0800

    修改版本号

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[c8d9623]"
-#define RELEASE_DATE  "[2015-06-16]"
+#define REVISION      "[f61ea18]"
+#define RELEASE_DATE  "[2015-06-17]"
