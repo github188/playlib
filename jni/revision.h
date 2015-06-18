82250b5410600ba8cc22a85a8207e37b823577f8
miaofaqiang <mfq@jovision.com>
2015-06-18 14:38:09 +0800

    修改版本号260b043

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[f61ea18]"
-#define RELEASE_DATE  "[2015-06-17]"
+#define REVISION      "[260b043]"
+#define RELEASE_DATE  "[2015-06-18]"
