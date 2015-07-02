422c596828f6494a6e9a83284dcf1cfa0bef160c
miaofaqiang <mfq@jovision.com>
2015-07-02 16:41:02 +0800

    修改播放库版本号：d1285af

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[82250b5]"
-#define RELEASE_DATE  "[2015-06-18]"
+#define REVISION      "[d1285af]"
+#define RELEASE_DATE  "[2015-07-02]"
