3c5d0cc430df35e97fe62e25b515d156b66ae54c
paul <songkunpeng@jovision.com>
2015-07-24 14:29:42 +0800

    更新版本号

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[2e0951a]"
-#define RELEASE_DATE  "[2015-07-23]"
+#define REVISION      "[f61cb1e]"
+#define RELEASE_DATE  "[2015-07-24]"
