548469c9b7d44b308a11e74b2121738a436df726
miaofaqiang <mfq@jovision.com>
2015-08-14 20:17:22 +0800

    解决网络库在移动环境下连接失败问题

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[82250b5]"
-#define RELEASE_DATE  "[2015-06-18]"
+#define REVISION      "[11ae8ec]"
+#define RELEASE_DATE  "[2015-08-14]"
