93a7b4f3c897b5596b455ea1387ae37768784dae
paul <songkunpeng@jovision.com>
2015-10-23 11:34:35 +0800

    修改release.sh

--- jni/release.sh
+++ jni/release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "//#define CASTRATE" > revision.h
 echo "#define MY_VERSION    \"0.9.1b\"" >> revision.h
 echo "#define REVISION      \"[`git rev-parse --short HEAD`]\"" >> revision.h
 echo "#define RELEASE_DATE  \"[`date +"%Y-%m-%d"`]\"" >> revision.h
--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[9258126]"
-#define RELEASE_DATE  "[2015-10-22]"
+#define REVISION      "[2531df7]"
+#define RELEASE_DATE  "[2015-10-23]"
