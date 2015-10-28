33f9dded569f2faddb0bfcd17f8edf2af3ae99e4
paul <songkunpeng@jovision.com>
2015-10-28 11:48:52 +0800

    使用nplayer3d36f03

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[fccfd21]"
-#define RELEASE_DATE  "[2015-10-27]"
+#define REVISION      "[1fa383e]"
+#define RELEASE_DATE  "[2015-10-28]"
