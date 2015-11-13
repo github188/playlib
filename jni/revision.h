8ccb098adb01ec95cb7ccfd78a3999ff864fdc3f
paul <songkunpeng@jovision.com>
2015-11-13 10:56:30 +0800

    打开网络库log

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[2f9ac08]"
-#define RELEASE_DATE  "[2015-09-18]"
+#define REVISION      "[d17125f]"
+#define RELEASE_DATE  "[2015-11-13]"
