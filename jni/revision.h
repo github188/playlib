162b742d39b7ffe0e418adf45bc02b53db4566dd
paul <songkunpeng@jovision.com>
2015-11-18 10:19:03 +0800

    修改版本信息

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
-#define MY_VERSION    "0.9.1b"
-#define REVISION      "[1113220]"
-#define RELEASE_DATE  "[2015-11-10]"
+#define MY_VERSION    "1.0.0"
+#define REVISION      "[b491816]"
+#define RELEASE_DATE  "[2015-11-16]"
