2dbb8052ddae3899cfca6f0cce26490e8c732e3b
paul <songkunpeng@jovision.com>
2015-11-19 16:07:27 +0800

    使用新网络库

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "0.9.1b"
-#define REVISION      "[d17125f]"
-#define RELEASE_DATE  "[2015-11-13]"
+#define REVISION      "[8ccb098]"
+#define RELEASE_DATE  "[2015-11-19]"
