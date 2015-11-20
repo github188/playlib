b97ef4258dfac164ec36159a69dc52413f72253c
paul <songkunpeng@jovision.com>
2015-11-20 09:50:33 +0800

    修改版本信息

--- jni/revision.h
+++ jni/revision.h
@@ -1,4 +1,4 @@
 //#define CASTRATE
 #define MY_VERSION    "1.0.0"
-#define REVISION      "[b491816]"
-#define RELEASE_DATE  "[2015-11-16]"
+#define REVISION      "[5a7dfac]"
+#define RELEASE_DATE  "[2015-11-20]"
